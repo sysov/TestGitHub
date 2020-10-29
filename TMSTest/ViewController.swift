5b29e2f8ab856e82c64a1b8218f235205734e11b
Valera Sysov <sysov@MacBook-Pro-Valera.local>
2020-10-29 20:05:26 +0300

    Initial commit

---
+++ TMSTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMSTest
+//
+//  Created by Valera Sysov on 10/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
